cdba1cd96c0d9478f591914481d5bfaba6c20159
Cody Poll <CJPoll@gmail.com>
2014-08-24 14:49:04 -0600

    The areas index api action is working correctly

--- spec/controllers/areas_controller.rb
+++
@@ -1,9 +0,0 @@
-describe 'Areas' do
-	context '/areas' do
-		area1 = FactoryGirl.create(:area)
-		get '/areas'
-		json = response.body
-		area = Area.build(JSON.parse(json))
-		expect(area).to_not eq(area1)
-	end
-end
---
+++ spec/controllers/areas_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe 'Areas' do
+	context '/areas' do
+		include RspecMixin
+		it "returns a stringified array of all areas the user can see" do
+			area = FactoryGirl.create(:area)
+			get '/areas'
+			json = last_response.body
+			areas = JSON.parse(json)
+			expect(areas.count).to eq(1)
+			expect(areas.first['id']).to eq(area.id)
+			expect(areas.first['name']).to eq(area.name)
+		end
+	end
+end
