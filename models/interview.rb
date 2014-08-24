b87f4198e3a61b945601528108687b5fa3f0db2f
Cody Poll <CJPoll@gmail.com>
2014-08-24 01:08:34 -0600

    Changed formatting of interview model

--- models/interview.rb
+++ models/interview.rb
@@ -45,7 +45,14 @@ class Interview < ActiveRecord::Base
 	belongs_to :household
 	has_many :consumed_foods
 
-	define_nutrients :calories, :sugars_grams, :sodium_grams, :fat_grams, :protein_grams, :cholesterol_grams, :vitamin_a_dv, :vitamin_c_dv
+	define_nutrients :calories,
+		:sugars_grams,
+		:sodium_grams,
+		:fat_grams,
+		:protein_grams,
+		:cholesterol_grams,
+		:vitamin_a_dv,
+		:vitamin_c_dv
 
 	validates_presence_of :consumed_foods
 end
