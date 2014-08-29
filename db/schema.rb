32d4dcfeefdedd3030015bc06dc226bc53b098bf
Cody Poll <CJPoll@gmail.com>
2014-08-29 18:40:51 -0400

    Forgot to include db/schema a couple commits ago when table structure changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828121107) do
+ActiveRecord::Schema.define(version: 20140829215624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20140828121107) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "area_id"
   end
 
   create_table "interviews", force: true do |t|
