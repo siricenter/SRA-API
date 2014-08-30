1af790d12074c088b791c39544351bff6eaea6e1
Cody Poll <CJPoll@gmail.com>
2014-08-29 20:15:52 -0400

    Users#index spec is passing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,17 @@
 describe 'Users' do
 	include RspecMixin
 	context '/users' do
+		it "returns a stringified array of all users the administrator can see" do
+			user = FactoryGirl.create(:user)
+			get '/users'
+			expect(last_response.status).to eq(200)
+
+			json = last_response.body
+			users_list = JSON.parse(json)
+			expect(users_list.count).to eq(1)
+			expect(User.new(users_list.first)).to eq(user)
+		end
+
 		it "creates a new User"
 	end
 
