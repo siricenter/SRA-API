9f18a5be8db16283a25b55b2e16d620a6af52bdd
frostyfire03530 <frostyfire03530@gmail.com>
2014-09-09 21:45:51 +0000

    Added schema information to the token.rb model file.

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tokens
+#
+#  id           :integer
+#  token_string :string
+#  user_id      :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Token < ActiveRecord::Base
 	validates :token_string, presence: true
 	belongs_to :user
