9ffa92c51aa7ea6f137593d00253767835fcb63c
andrewf2 <andrewf02@gmail.com>
2014-09-10 21:24:14 +0000

    providing documentation for the family relationships

--- routes/family_relationships.rb
+++ routes/family_relationships.rb
@@ -4,6 +4,7 @@ module Sinatra
             module FamilyRelationships
             
                 def self.registered(app)
+					#creates a new family_relationship
 					app.post "/family_relationships" do
 						relationship = FamilyRelationship.new(params[:family_relationship])
 						relationship.save
@@ -13,7 +14,7 @@ module Sinatra
 							return 403
 						end
 					end
-					
+					#deletes a family_relationship
 					app.delete "/family_relationship/:id" do
 						relationship = FamilyRelationship.find(params[:id])
 						relationship.delete
@@ -23,14 +24,15 @@ module Sinatra
 							return 400
 						end
 					end
-					
+					#returns all the existing family_relationships
 					app.get "/family_relationships" do
 						relationships = FamilyRelationship.all.to_json
 					end
-					
+					#returns a specfic family_relationship
 					app.get "/family_relationships/:id" do
 						relationship = FamilyRelationship.find(params[:id]).to_json
 					end
+					#updating a specific family_relationship
 					app.put "/family_relationships/:id" do
 						relationship = FamilyRelationship.find(params[:id])
 						relationship = params[:family_relationship]
