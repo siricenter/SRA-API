ea98a654e4f4b6dcf4ebebbe830ac8c90e5a8b6f
andrewf2 <andrewf02@gmail.com>
2014-09-10 19:43:48 +0000

    implemented the route for areas_users

--- routes/areas_users.rb
+++ routes/areas_users.rb
@@ -4,7 +4,31 @@ module Sinatra
             module AreasUsers
             
                 def self.registered(app)
-                    
+					#Creates a new relationship
+					app.post '/areas/:area_id/users/:user_id' do
+						area = Area.find(params[:area_id])
+						user = User.find(params[:user_id])
+        				relationship = params[:relationship]
+						area_user = AreaRelationship.new
+        				area_user.area = area
+       					area_user.user = user
+       					area_user.relationship = relationship
+						area_user.save
+						if area_user.save 
+							return 200
+						else
+							return 403
+					end
+						#Deletes a relationship
+					app.delete '/areas/:area_id/users/:user_id' do
+						area = Area.find(params[:area_id])
+						user = User.find(params[:user_id])
+						area.users.delete(user) if area.users.include?(user)
+							if area.delete
+								return 200
+							else
+								return 403
+						end
                 end
             
             end
