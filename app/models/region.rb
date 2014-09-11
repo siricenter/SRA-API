952ccb7c34214a76f27776e8fc5b3246372333d3
frostyfire03530 <frostyfire03530@gmail.com>
2014-09-11 18:45:07 +0000

    Changed the region.rb model from 'has many' to 'has_many'.

--- app/models/region.rb
+++ app/models/region.rb
@@ -11,5 +11,5 @@
 class Region < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_and_belongs_to_many :areas, through: :areas_regions
-  has many :areas_regions
+  has_many :areas_regions
 end
