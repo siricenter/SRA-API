db3cb7dc8d9028e7cd3470fc31e0c608283f42ed
andrewf2 <andrewf02@gmail.com>
2014-09-11 19:57:15 +0000

    added consumed foods factory

--- spec/factories/consumed_foods.rb
+++ spec/factories/consumed_foods.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-	factory :consumed_food do
-		n_id "MyString"
-		servings 1
-		frequency "daily"
+    factory :consumed_food do |factory|
+		n_id "513fceb575b8dbbc21001506"
+		servings 3
+		frequency 'daily'
 	end
 end
