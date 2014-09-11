91a42780057874f7d30b1dfd3288b88ecbdd542a
andrewf2 <andrewf02@gmail.com>
2014-09-11 18:56:52 +0000

    wrote spec for people controller

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,19 +1,38 @@
 describe 'People' do
 	include RspecMixin
 	context '/people' do
-		it "returns a stringified array of all  the use" do
-			interview = FactoryGirl.create(:interview)
-			get '/interviews'
+		it "returns a stringified array of all people" do
+			person = FactoryGirl.create(:person)
+			job = FactoryGirl.create(:job, {person: person})
+			get '/people'
 			json = last_response.body
-			interviews = JSON.parse(json)
-			retrieved_interview = Interview.new(interviews.first)
-			expect(retrieved_interview).to eq(interview)
+			person_js = JSON.parse(json)
+			expect(person_js).to eq(person)	
+			expect(person_js.first['job']).to eq(job)
 		end
 
-		it "creates a new interview" do
+		it "creates a new person_js" do
 			expect {
-				post '/interviews', {interview: {id: 1}}
-			}.to change(Interview, :count).by(1)
+				post '/person', {person: {id: 1}}
+				}.to change(Person, :count).by(1)
+		end
+		
+		it "deletes a person" do
+			person = FactoryGirl.create(:person)
+			person.save
+			delete "/people/#{person.to_param}"
+			person.destroy
+			expect(person.count).to eq(0)
+		end
+		it 'updates a person' do
+			person = FactoryGirl.create(:person)
+			put "/people/#{person.to_param}"
+			person = params[:person => {give_name: 'Willy', family_name: 'Wonka',education_level: 'None', gender: 'female'}]
+			person.save
+			expect(person[:given_name]).to eq('Willy')
+			expect(person[:family_name]).to eq('Wonka')
+			expect(person[:education_level]).to eq('None')
+			expect(person[:gender]).to eq('female')
 		end
 
 	end
