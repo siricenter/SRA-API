b7d649c432a166e2113888ef647c1e5294c8bdad
andrewf2 <andrewf02@gmail.com>
2014-10-06 18:05:35 +0000

    the auth spec is passing without key and token values for prototype. All the mobile app will require for now is the  username and password.

--- routes/sessions.rb
+++ routes/sessions.rb
@@ -4,22 +4,21 @@ module Sinatra
             module Sessions
             
                 def self.registered(app)
-                    
                     #Create a session
                     app.post '/session' do
-                        return 403 unless params[:key]
+                        #return 403 unless params[:key]
                         return 403 unless params[:user] and params[:user][:email] and params[:user][:password]
                         user = User.where(email: params[:user][:email]).first
                         return 403 unless user.password == params[:user][:password]
-                        return 403 unless ApiKey.exists?(key: params[:key])
-                        uuid = UUID.new.generate
-                        token = Token.new({token_string: uuid, user_id: user.id})
-                        token.save!
-                        return token.token_string
+                        #return 403 unless ApiKey.exists?(key: params[:key])
+                        #uuid = UUID.new.generate
+                        #token = Token.new({token_string: uuid, user_id: user.id})
+                        #token.save!
+                        #return token.token_string
+						
+						
                     end
-                    
                 end
-            
             end
         end
     end
--- spec/controllers/auth_spec.rb
+++ spec/controllers/auth_spec.rb
@@ -4,9 +4,9 @@ describe 'auth' do
 	describe 'login' do
 		before :each do
 			@user = FactoryGirl.create(:user)
-            api_key = FactoryGirl.create(:api_key)
+            #api_key = FactoryGirl.create(:api_key)
 			@params = {
-                key: api_key.key, 
+                #key: api_key.key, 
 				user: {
 					email: @user.email,
 					password: @user.password
@@ -19,17 +19,17 @@ describe 'auth' do
 			expect(last_response.status).to eq(200)
 		end
 
-        it "requires an api key" do
-			@params[:key] = nil
-			post '/session', @params
-			expect(last_response.status).to eq(403)
-		end
+       # it "requires an api key" do
+		#	@params[:key] = nil
+		#	post '/session', @params
+		#	expect(last_response.status).to eq(403)
+	#	end
         
-        it "requires an api key to be valid" do
-            @params[:key] = FactoryGirl.build(:api_key)
-            post '/session', @params
-			expect(last_response.status).to eq(403)
-		end
+       # it "requires an api key to be valid" do
+         #   @params[:key] = FactoryGirl.build(:api_key)
+         #   post '/session', @params
+		#	expect(last_response.status).to eq(403)
+		#end
 
 		it "requires login credentials to work" do
 			@params[:user] = nil
@@ -49,17 +49,17 @@ describe 'auth' do
 			expect(last_response.status).to eq(403)
 		end
 
-		it "returns a randomly generated token" do
+		#it "returns a randomly generated token" do
 			# For now, we'll just use a hard coded value
-			post '/session', @params
-            expect(last_response.body).to match(/\A\w{8}[-]\w{4}[-]\w{4}[-]\w{4}[-]\w{12}\z/)
-		end  
+		#	post '/session', @params
+         #   expect(last_response.body).to match(/\A\w{8}[-]\w{4}[-]\w{4}[-]\w{4}[-]\w{12}\z/)
+		#end  
 
-		it "stores the given token in a database" do
-			expect {
-				post '/session', @params
-			}.to change(Token, :count).by(1)
-		end
+		#it "stores the given token in a database" do
+		#	expect {
+		#		post '/session', @params
+		#	}.to change(Token, :count).by(1)
+		#end
 
 		it 'requires the email/password combination to be valid' do
 			@params[:user][:password] = 'randomwrongpassword01'
@@ -67,9 +67,9 @@ describe 'auth' do
 			expect(last_response.status).to eq(403)
 		end
 
-		it "associates the token with a user account" do
-			post '/session', @params
-			expect(Token.last.user).to eq(@user)
-		end
+		#it "associates the token with a user account" do
+		#	post '/session', @params
+		#	expect(Token.last.user).to eq(@user)
+		#end
 	end
 end
