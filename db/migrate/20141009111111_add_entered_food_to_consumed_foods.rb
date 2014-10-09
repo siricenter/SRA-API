6f66a93e829af24f3ba629698c1eb3fb4aef8d42
frostyfire03530 <frostyfire03530@gmail.com>
2014-10-09 21:58:22 +0000

    Added migration for consumed_foods to have entered_food column.

---
+++ db/migrate/20141009111111_add_entered_food_to_consumed_foods.rb
@@ -0,0 +1,11 @@
+class AddEnteredFoodToConsumedFoods < ActiveRecord::Migration
+	def up
+        add_column :consumed_foods, :entered_food, :string
+		end
+	end
+
+	def down
+        remove_column :consumed_foods, :entered_food
+		end
+	end
+end
