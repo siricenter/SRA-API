e2626b5b9545399cdf80271439470e92998703e7
andrewf2 <andrewf02@gmail.com>
2014-10-09 22:10:04 +0000

    fixed spec so it wasnt breaking everything

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,7 +3,7 @@ describe 'Consumed Foods' do
 	context '/consumed_foods' do
 		it "returns a list of food" do
 			query = 'corn'
-			post '/search' {query: query}
+			post '/search', {query: query}
 			json = last_response.body
 			results = JSON.parse(json)
 			expect(results.n_id).should_not eq(0)
