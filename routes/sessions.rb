fb381b28f70abcb1530c85325bbe24f080ef4b81
andrewf2 <andrewf02@gmail.com>
2014-11-04 22:05:11 +0000

    cleaned up git mess in sessions route

--- routes/sessions.rb
+++ routes/sessions.rb
@@ -10,19 +10,13 @@ module Sinatra
                         user = User.where(email: params[:user][:email]).first
                         return 403 unless user.password == params[:user][:password]
                         #return 403 unless ApiKey.exists?(key: params[:key])
-<<<<<<< HEAD
                         uuid = UUID.new.generate
                         token = Token.new({token_string: uuid, user_id: user.id})
                         token.save!
                         return token.token_string
 						
 						
-=======
-                        #uuid = UUID.new.generate
-                        #token = Token.new({token_string: uuid, user_id: user.id})
-                        #token.save!
-                        #return token.token_string
->>>>>>> b613f1bb7ade94ed627f976b0c128adabb620923
+
                     end
                 end
             end
